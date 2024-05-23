bec7af0fe0edc7cffe5718b584d3b6d75da75b8c
Rudi MK <rudimk.cloud@gmail.com>
2024-05-23 16:06:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"author": "rudimk", "message": "in remembrance of earth's past."}
+
+@app.get("/info")
+async def root():
+    return {"author": "rudimk", "message": "สวัสดี"}
